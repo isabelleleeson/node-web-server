d99316ca963d77db15f4566ae42cec5beb80f297
Isabelle Leeson <isabelleleeson@Isabelles-MacBook-Pro.local>
2017-01-31 00:49:07 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express'); // also express middlewear
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); // creats app
+
+hbs.registerPartials(__dirname + '/views/partials/')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {// next exists so you can tell express when your middlewear fn is done
+    var now = new Date().toString();
+    
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) console.log('Unable to append to server.log.');
+    });
+    next();
+});
+
+//app.use((req,res,next) => {
+//    res.render('maintenance.hbs');
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    var year = new Date().getFullYear();
+    return year;
+    //return 'test';
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my bomb-ass website!'
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+//binds application to host on machine
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+}); // common port for developping locally
